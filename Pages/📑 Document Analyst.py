f61039f201c8dc8998dbcdd0b78a58e36acbd3cd
jkmathilda <jkmathilda627@gmail.com>
2024-01-23 00:21:29 -0500

    Update DocumentAnalyst.py

--- Pages/📑 Document Analyst.py
+++ Pages/📑 Document Analyst.py
@@ -5,10 +5,23 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage
 from langchain.callbacks.base import BaseCallbackHandler
 import baseFunction as bf
+from PyPDF2 import PdfReader
 
 
 def main():
-    pass
+    st.header('Analyze PDF 📑')
+    
+    pdf = st.file_uploader("Upload your PDF", type="pdf")  # Create a file uploader widget specifically for PDF files
+    cancel_button = st.button('Cancel')
+    if cancel_button:
+        st.stop()
+        
+    # Extract text from the PDF
+    if pdf is not None:                     # Check if a PDF file has been successfully uploaded
+        pdf_reader = PdfReader(pdf)         # Initialize a PDF reader to read the uploaded file
+        pdf_text = ""                           # Initialize a string to accumulate extracted text
+        for page in pdf_reader.pages:       # Loop through each page in the PDF
+            pdf_text += page.extract_text()     # Append the extracted text from each page to the 'text' variable
 
 
 if __name__ == "__main__":
